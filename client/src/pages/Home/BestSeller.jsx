03d2fcfca1a870d71be28c8dcfe54ce2658e9a3a
deepkx <avanthuram@gmail.com>
2024-04-18 16:57:55 +0530

    Added changes in api call

--- client/src/pages/Home/BestSeller.jsx
+++ client/src/pages/Home/BestSeller.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import BookCards from '../shared/BookCards';
-
+import axios from 'axios'; 
 const BestSeller = () => {
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => setBooks(data.slice(0, 8)))
+
+        fetch("'https://bookflix-server.vercel.app/api/all-books'").then(res => res.json()).then(data => setBooks(data.slice(0, 8)))
+
+        // fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => setBooks(data.slice(0, 8)))
     }, [])
 
     return (
